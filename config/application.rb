81b026b8f656e49629f64b518deb1dad0cd23b49
Richard Wise <orsusbass@gmail.com>
2021-03-09 22:46:24 -0600

    remove unneeded scss

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,12 @@ module Jessflexapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
+    # prevent specified generators from running
+    config.generators do |g|
+      g.stylesheets false
+      g.helper    false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
