4143e43a327962a6314e07dc18d76fe44fea0bdf
Richard Wise <orsusbass@gmail.com>
2021-03-15 15:30:56 -0500

    linter updates

--- spec/models/breadcrumb_spec.rb
+++ spec/models/breadcrumb_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe "Breadcrumb", type: :model do
     context "without a path" do
       it "will return false" do
         breadcrumb = Breadcrumb.new("Test", nil)
-        expect(breadcrumb.link?).to eq(false) 
+        expect(breadcrumb.link?).to eq(false)
       end
     end
 
     context "with a path" do
       it "will return true" do
         breadcrumb = Breadcrumb.new("Test", "/admin/dashboard")
-        expect(breadcrumb.link?).to eq(true) 
+        expect(breadcrumb.link?).to eq(true)
       end
     end
   end
