f4648c179e41cb4db9906ffb14d70fdc7e6d61df
Richard Wise <orsusbass@gmail.com>
2021-03-15 15:17:00 -0500

    add tests for breadcrumb

---
+++ spec/models/breadcrumb_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "Breadcrumb", type: :model do
+  describe "#link?" do
+    context "without a path" do
+      it "will return false" do
+        breadcrumb = Breadcrumb.new("Test", nil)
+        expect(breadcrumb.link?).to eq(false) 
+      end
+    end
+
+    context "with a path" do
+      it "will return true" do
+        breadcrumb = Breadcrumb.new("Test", "/admin/dashboard")
+        expect(breadcrumb.link?).to eq(true) 
+      end
+    end
+  end
+end
